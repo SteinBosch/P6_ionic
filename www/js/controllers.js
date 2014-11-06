e13544ac3d17b4b21c9bfcd0e1988ee0a3ce8509
Rubinho <ruben@rkmediadesign.nl>
2014-11-06 21:01:07 +0100

    Cleaned up controller file and removed some unused controllers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,46 +1,6 @@
-var oefeningen = [
-    { oefening: 'Naar de wc gaan; zitten op de bril en niet mijn handen wassen en een broodje eten',
-      id: 1,
-      score: 100,
-      details: {
-        all_scores: [80, 60, 40],
-         backlog: [
-          {date: "10 mei 2014", fear_score: 80, log: "Ik ben niet ziek geworden van een broodje eten met ongewasen handen nadat ik had gezet. Dus dat is fijn.",},
-          {date: "16 mei 2014", fear_score: 60, log: "Omdat ik vorgie keer niet ziek ben geworden ging het me een stuk beter af. Ik rook nog aan mijn handen maar deze stonken niet.",},
-          {date: "21 mei 2014", fear_score: 40, log: "Het gaat steeds beter en vind het minder erg!",},
-        ],
-      },
-    },
-    { oefening: 'Naar de wc gaan, zitten op de bril daarna nog handen wassen op niet dwangmatige manier', id: 2, score: 90,
-      details: {
-        all_scores: [90, 50, 70, 30, 30],
-         backlog: [
-          {date: "11 mei 2014", fear_score: 90, log: "Heb mijn handen wel gewassen maar want vond de wc erg smering",},
-          {date: "17 mei 2014", fear_score: 50, log: "Ben op de bril gaan zitten maar nu was de wc schoon gemaakt dus vond ik het minder erg. Ik zag geen smerige stoffen.",},
-          {date: "22 mei 2014", fear_score: 70, log: "Ik heb mijn handen goed gewassen omdat het er wel vies uitzag, maar omdat de vorige keer erger was vond ik het nu beter gaan.",},
-        ],
-      }, },
-    { oefening: 'In de wc. aanraken van kranen, papierrol, prullenbak enz. niet handen wassen', id: 3, score: 80 },
-    { oefening: 'In het ziekenhuis een broodje eten ', id: 4, score: 70 },
-    { oefening: 'Een broodje eten in het winkelcentrum na het aanraken van stoplichtknopjes zonder mijn handen te wassen', id: 5, score: 60 },
-    { oefening: 'Pinnen bij het winkelcentrum en niet mijn handen wassen', id: 6, score: 50,
-      details: {
-        all_scores: [50, 50, 70, 10, 90, 20, 60],
-         backlog: [
-          {date: "11 mei 2014", fear_score: 90, log: "Ik ben niet ziek geworden van de vorige keer met pinnen, dus ik had nu minder het gevoel dat het kwaad zou kunnen. Ik heb zelfs met m’n andere hand de automaat nog aangeraakt!",},
-          {date: "17 mei 2014", fear_score: 50, log: "Er stond iemand voor me die er onhygienisch uitzag, dus ik durfde eigenlijk de toetsen niet aan te raken. Maar ik bedacht me dat het alleen maar door m’n dwang kwam en toen heb ik alsnog gepind.",},
-          {date: "22 mei 2014", fear_score: 70, log: "4Ik ben niet ziek geworden van de vorige keer met pinnen.",},
-        ],
-      }, },
-    { oefening: 'Naar de wc gaan als ik moet (dus niet vermijden door ophouden)', id: 7, score: 40 },
-    { oefening: 'Aanraken van de kranen op de wc en niet mijn handen wassen', id: 8, score: 30 },
-    { oefening: 'Aanraken van de deurknoppen van de wc en niet mijn handen wassen', id: 9, score: 20 },
-    { oefening: 'Aanraken van de deurknoppen en trapleuningen en niet mijn handen wassen', id: 10, score: 10 },
-  ];
-  //console.log(oefeningen);
-
 angular.module('starter.controllers', [])
 
+// DEZE CONTROLLER MOET HERNOEMD EN GAAT DIT WERKEN MET SILVERSTRIPE?
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
   // Form data for the login modal
   $scope.loginData = {};
@@ -74,55 +34,16 @@ angular.module('starter.controllers', [])
   };
 })
 
+// DEZE TWEE MOETEN NOG SAMENGEVOEGD WORDEN
 .controller('OefeningenCtrl', function($scope) {
-  $scope.oefeningen = oefeningen;
+  $scope.oefeningen = [{ oefening: 'Naar de wc gaan; zitten op de bril en niet mijn handen wassen en een broodje eten', id: 1, score: 100, details: {all_scores: [80, 60, 40], backlog: [{date: "10 mei 2014", fear_score: 80, log: "Ik ben niet ziek geworden van een broodje eten met ongewasen handen nadat ik had gezet. Dus dat is fijn.",}, {date: "16 mei 2014", fear_score: 60, log: "Omdat ik vorgie keer niet ziek ben geworden ging het me een stuk beter af. Ik rook nog aan mijn handen maar deze stonken niet.",}, {date: "21 mei 2014", fear_score: 40, log: "Het gaat steeds beter en vind het minder erg!",}, ], }, }, { oefening: 'Naar de wc gaan, zitten op de bril daarna nog handen wassen op niet dwangmatige manier', id: 2, score: 90, details: {all_scores: [90, 50, 70, 30, 30], backlog: [{date: "11 mei 2014", fear_score: 90, log: "Heb mijn handen wel gewassen maar want vond de wc erg smering",}, {date: "17 mei 2014", fear_score: 50, log: "Ben op de bril gaan zitten maar nu was de wc schoon gemaakt dus vond ik het minder erg. Ik zag geen smerige stoffen.",}, {date: "22 mei 2014", fear_score: 70, log: "Ik heb mijn handen goed gewassen omdat het er wel vies uitzag, maar omdat de vorige keer erger was vond ik het nu beter gaan.",}, ], }, }, { oefening: 'In de wc. aanraken van kranen, papierrol, prullenbak enz. niet handen wassen', id: 3, score: 80 }, { oefening: 'In het ziekenhuis een broodje eten ', id: 4, score: 70 }, { oefening: 'Een broodje eten in het winkelcentrum na het aanraken van stoplichtknopjes zonder mijn handen te wassen', id: 5, score: 60 }, { oefening: 'Pinnen bij het winkelcentrum en niet mijn handen wassen', id: 6, score: 50, details: {all_scores: [50, 50, 70, 10, 90, 20, 60], backlog: [{date: "11 mei 2014", fear_score: 90, log: "Ik ben niet ziek geworden van de vorige keer met pinnen, dus ik had nu minder het gevoel dat het kwaad zou kunnen. Ik heb zelfs met m’n andere hand de automaat nog aangeraakt!",}, {date: "17 mei 2014", fear_score: 50, log: "Er stond iemand voor me die er onhygienisch uitzag, dus ik durfde eigenlijk de toetsen niet aan te raken. Maar ik bedacht me dat het alleen maar door m’n dwang kwam en toen heb ik alsnog gepind.",}, {date: "22 mei 2014", fear_score: 70, log: "4Ik ben niet ziek geworden van de vorige keer met pinnen.",}, ], }, }, { oefening: 'Naar de wc gaan als ik moet (dus niet vermijden door ophouden)', id: 7, score: 40 }, { oefening: 'Aanraken van de kranen op de wc en niet mijn handen wassen', id: 8, score: 30 }, { oefening: 'Aanraken van de deurknoppen van de wc en niet mijn handen wassen', id: 9, score: 20 }, { oefening: 'Aanraken van de deurknoppen en trapleuningen en niet mijn handen wassen', id: 10, score: 10 }];
 })
 
 .controller('OefeningCtrl', function($scope, $stateParams) {
-  $scope.oefeningen = oefeningen;
+  $scope.oefeningen = [{ oefening: 'Naar de wc gaan; zitten op de bril en niet mijn handen wassen en een broodje eten', id: 1, score: 100, details: {all_scores: [80, 60, 40], backlog: [{date: "10 mei 2014", fear_score: 80, log: "Ik ben niet ziek geworden van een broodje eten met ongewasen handen nadat ik had gezet. Dus dat is fijn.",}, {date: "16 mei 2014", fear_score: 60, log: "Omdat ik vorgie keer niet ziek ben geworden ging het me een stuk beter af. Ik rook nog aan mijn handen maar deze stonken niet.",}, {date: "21 mei 2014", fear_score: 40, log: "Het gaat steeds beter en vind het minder erg!",}, ], }, }, { oefening: 'Naar de wc gaan, zitten op de bril daarna nog handen wassen op niet dwangmatige manier', id: 2, score: 90, details: {all_scores: [90, 50, 70, 30, 30], backlog: [{date: "11 mei 2014", fear_score: 90, log: "Heb mijn handen wel gewassen maar want vond de wc erg smering",}, {date: "17 mei 2014", fear_score: 50, log: "Ben op de bril gaan zitten maar nu was de wc schoon gemaakt dus vond ik het minder erg. Ik zag geen smerige stoffen.",}, {date: "22 mei 2014", fear_score: 70, log: "Ik heb mijn handen goed gewassen omdat het er wel vies uitzag, maar omdat de vorige keer erger was vond ik het nu beter gaan.",}, ], }, }, { oefening: 'In de wc. aanraken van kranen, papierrol, prullenbak enz. niet handen wassen', id: 3, score: 80 }, { oefening: 'In het ziekenhuis een broodje eten ', id: 4, score: 70 }, { oefening: 'Een broodje eten in het winkelcentrum na het aanraken van stoplichtknopjes zonder mijn handen te wassen', id: 5, score: 60 }, { oefening: 'Pinnen bij het winkelcentrum en niet mijn handen wassen', id: 6, score: 50, details: {all_scores: [50, 50, 70, 10, 90, 20, 60], backlog: [{date: "11 mei 2014", fear_score: 90, log: "Ik ben niet ziek geworden van de vorige keer met pinnen, dus ik had nu minder het gevoel dat het kwaad zou kunnen. Ik heb zelfs met m’n andere hand de automaat nog aangeraakt!",}, {date: "17 mei 2014", fear_score: 50, log: "Er stond iemand voor me die er onhygienisch uitzag, dus ik durfde eigenlijk de toetsen niet aan te raken. Maar ik bedacht me dat het alleen maar door m’n dwang kwam en toen heb ik alsnog gepind.",}, {date: "22 mei 2014", fear_score: 70, log: "4Ik ben niet ziek geworden van de vorige keer met pinnen.",}, ], }, }, { oefening: 'Naar de wc gaan als ik moet (dus niet vermijden door ophouden)', id: 7, score: 40 }, { oefening: 'Aanraken van de kranen op de wc en niet mijn handen wassen', id: 8, score: 30 }, { oefening: 'Aanraken van de deurknoppen van de wc en niet mijn handen wassen', id: 9, score: 20 }, { oefening: 'Aanraken van de deurknoppen en trapleuningen en niet mijn handen wassen', id: 10, score: 10 }];
   $scope.stateParams = [
     {oefeningId: $stateParams['oefeningId'] -1,},
   ]
 })
-
-.controller('serverCtrl',function ($scope,$http){
- $http.get('/server/json.php').then(function(resp) {
-    console.log(resp.data);
-    $scope.posts = resp.data;
-    console.log($scope.posts);
-    // For JSON responses, resp.data contains the result
-  }, function(err) {
-    console.error('ERR', err);
-    // err.status will contain the status code
-  });
-  
-})
-
-.controller('patientCtrl',function ($scope,$http){
-  var url = "http://backend-p6:8888/api/TherapyGroup";
-  $http.get(url)
-      .success(function(data){
-          console.log(data);
-          $scope.list = data;
-      });
-  
-})
-
-.controller('postCtrl',function ($scope,$http){
-      $scope.master = {};
-
-      $scope.input = function(input) {
-        $scope.master = angular.copy(input);
-        console.log($scope.master);
-
-        $http.post("server/insert.php",{'post': $scope.master.text})
-        .success(function(data, status, headers, config){
-            console.log("inserted Successfully");
-            console.log(data); 
-        });
-      };
-
-    // }]);
-});
 
 ;
